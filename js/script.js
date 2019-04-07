16277721e20ccd51e7acf0d5a44cf08c48af535f
Diego Ramirez <rambay1990@gmail.com>
2019-04-07 02:16:59 -0500

    page quiz add

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const menu = document.getElementById('menuBurguer'),
       navMenu = document.getElementById('menuNav');
 
+const tool = document.getElementById('tooltip');
+
 const ipad = window.matchMedia('screen and (max-width: 800px)');
 
 ipad.addListener(validacion);
@@ -20,3 +22,11 @@ function showMenu() {
   menu.classList.toggle('is-active');
   navMenu.classList.toggle('menu-active')
 };
+
+function toolTipActive() {
+ tool.classList.add('tooltip-active');
+}
+
+function toolTipFalse() {
+  tool.classList.remove('tooltip-active');
+ }
