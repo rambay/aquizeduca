7d0a8f879282384c21d236b3a303418b2c524fdf
Diego Ramirez <rambay1990@gmail.com>
2019-04-07 17:51:53 -0500

    fix small bugs

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 const menu = document.getElementById('menuBurguer'),
       navMenu = document.getElementById('menuNav');
 
-const tool = document.getElementById('tooltip');
-
 const ipad = window.matchMedia('screen and (max-width: 800px)');
 
 ipad.addListener(validacion);
@@ -22,11 +20,3 @@ function showMenu() {
   menu.classList.toggle('is-active');
   navMenu.classList.toggle('menu-active')
 };
-
-function toolTipActive() {
- tool.classList.add('tooltip-active');
-}
-
-function toolTipFalse() {
-  tool.classList.remove('tooltip-active');
- }
