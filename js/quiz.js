b52e8bfe1a78bfef6c9f2f3d80e03bb45e6f12ca
Diego Ramirez <rambay1990@gmail.com>
2019-04-11 17:12:41 -0500

    quiz solution

--- js/quiz.js
+++ js/quiz.js
@@ -18,6 +18,12 @@ let respuestas = [
   ['control de versiones', 'plataforma', 'comunidad']
 ];
 
+let pointsCorrectJugadorUno = 0;
+let pointsIncorrectJugadorUno = 0;
+
+let pointsCorrectJugadorDos = 0;
+let pointsIncorrectJugadorDos = 0;
+
 // INDICADOR PUNTOS JUGADORES
 
 var acertoJugada = 25;
@@ -29,30 +35,13 @@ var acumPuntosJugadorDos = 0;
 
 var indicarInicial = 1;
 
+
 // NIVEL DE PREGUNTA
 
 var level = 0;
 
-// JUGADORES
-
-jugadorUnoActivo = true;
-jugadorDosActivo = false;
-
-let pointsCorrect = 0;
-let pointsIncorrect = 0;
-
-function jugadorUno() {
-  questionOrder.innerHTML = ++indicarInicial + '/3';
-  acumPuntosJugadorUno += acertoJugada;
-  ++pointsCorrect;
-  jugar(level++);
-}
-
-function jugadorDos() {
-  questionOrder.innerHTML = ++indicarInicial + '/3';
-  acumPuntosJugadorDos += acertoJugada;
-  ++pointsCorrect;
-  jugar(level++);
+function posicionQuestion() {
+  questionOrder.textContent = `${indicarInicial++}/3`;
 }
 
 function templateModalResult(playerPhoto, playerName, pointTotal, pointCorrect, pointIncorrect) {
@@ -76,15 +65,52 @@ function templateModalResult(playerPhoto, playerName, pointTotal, pointCorrect, 
   </div>`);
 }
 
-function winPlayer() {
+function showModal() {
   modalResult.classList.add('active-modalResult');
 }
 
-jugar(level);
+empezarJuego(level);
 
-function jugar() {
+posicionQuestion();
 
-  var indice_respuesta_correcta;
+var indice_respuesta_correcta;
+var respuesta;
+
+var jugadorUnoTurno = true;
+var jugadorDosTurno = false;
+
+function empezarJuego() {
+
+  function jugadorUno() {
+    if (respuesta === indice_respuesta_correcta) {
+      console.log('Correcto jugador1');
+      acumPuntosJugadorUno += acertoJugada;
+      pointsCorrectJugadorUno += 1;
+      empezarJuego(level++);
+      posicionQuestion();
+
+    } else {
+      console.log('Incorrecto jugador1');
+      jugadorUnoTurno = false;
+      jugadorDosTurno = true;
+      pointsIncorrectJugadorDos += 1;
+    }
+  }
+
+  function jugadorDos() {
+    if (respuesta === indice_respuesta_correcta) {
+      console.log('Correcto jugador2');
+      acumPuntosJugadorDos += acertoJugada;
+      pointsCorrectJugadorDos += 1;
+      empezarJuego(level++);
+      posicionQuestion();
+    } else {
+      console.log('Incorrecto jugador2');
+      jugadorDosTurno = false;
+      jugadorUnoTurno = true;
+      pointsIncorrectJugadorDos += 1;
+    }
+  }
 
   var indiceAleatorio = Math.floor(Math.random() * questions.length);
 
@@ -97,7 +123,7 @@ function jugar() {
   var posicion_aleatoria;
   for (i in respuestas_posibles) {
     posicion_aleatoria = Math.floor(Math.random() * posiciones.length);
-    if (posicion_aleatoria == 0 && ya_se_metio == false) {
+    if (posicion_aleatoria === 0 && ya_se_metio == false) {
       indice_respuesta_correcta = i;
       ya_se_metio = true;
     }
@@ -107,7 +133,7 @@ function jugar() {
 
   var txt_respuestas = '';
   for (i in respuestas_reordenadas) {
-    txt_respuestas += '<button class="answers__full" value="' + i + '">' + respuestas_reordenadas[i] + '</button>';
+    txt_respuestas += '<button class="answers__full" data-value="' + i + '">' + respuestas_reordenadas[i] + '</button>';
   }
 
   let applyImage = "";
@@ -132,42 +158,43 @@ function jugar() {
 
   //VERIFICAR LOS BOTONES DE RESPUESTAS
 
-  $(".answers__full").click(function () {
-
-    var respuesta = $(this).val();
-    if (respuesta == indice_respuesta_correcta && jugadorUnoActivo == true) {
-      jugadorUno();
-    }
-    else if (jugadorDosActivo == true) {
-      jugadorDos();
-      jugadorUnoActivo = false;
-      jugadorDosActivo = true;
-    }
-    else {
-      jugadorUnoActivo = false;
-      jugadorDosActivo = true;
-      ++pointsIncorrect;
-    }
-  });
-
   pointsPlayerOne.innerHTML = acumPuntosJugadorUno + ' ' + 'pts';
   pointsPlayerTwo.innerHTML = acumPuntosJugadorDos + ' ' + 'pts';
 
   if (indicarInicial > 3 && acumPuntosJugadorUno > acumPuntosJugadorDos) {
-    modalResult.innerHTML = templateModalResult('user01', 'Diego', acumPuntosJugadorUno, pointsCorrect, pointsIncorrect);
-    winPlayer();
+    modalResult.innerHTML = templateModalResult('user01', 'Diego', acumPuntosJugadorUno, pointsCorrectJugadorUno, pointsIncorrectJugadorUno);
+    showModal();
     level = 0;
     indicarInicial = 1;
     questionOrder.innerHTML = indicarInicial + '/3';
-    jugar(level);
+    empezarJuego(level);
   }
   else if (indicarInicial > 3 && acumPuntosJugadorDos > acumPuntosJugadorUno) {
-    modalResult.innerHTML = templateModalResult('user02', 'Luis', acumPuntosJugadorDos, pointsCorrect, pointsIncorrect);
-    winPlayer();
+    modalResult.innerHTML = templateModalResult('user02', 'Luis', acumPuntosJugadorDos, pointsCorrectJugadorDos, pointsIncorrectJugadorDos);
+    showModal();
     level = 0;
     indicarInicial = 1;
     questionOrder.innerHTML = indicarInicial + '/3';
-    jugar(level);
+    empezarJuego(level);
   }
+
+  var buttonAnswer = document.querySelectorAll('.answers__full');
+
+  buttonAnswer.forEach(i => {
+    i.addEventListener('click', () => {
+      respuesta = i.dataset.value;
+      if (jugadorUnoTurno === true) {
+        jugadorUno();
+      }
+
+      else if (jugadorDosTurno === true) {
+        jugadorDos();
+      }
+    })
+  });
+
+
+
 }
+
 
